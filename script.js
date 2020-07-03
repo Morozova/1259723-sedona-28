e95a3db65f6eded99101be338a05e3c2dc445b4e
Morozova <vmorozova08@gmail.com>
2020-07-03 14:48:10 +0300

    Дополнено

---
+++ script.js
@@ -0,0 +1,10 @@
+var selectionHotel = document.querySelector(".form-call-button");
+var formPopup = document.querySelector(".form-container");
+
+selectionHotel.addEventListener("click", function () {
+  formPopup.classList.toggle("visually-hidden");
+});
+
+
+
+
