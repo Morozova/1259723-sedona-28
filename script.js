5cef53441793f07b878a116ffabdadcb7ee92e69
Morozova <vmorozova08@gmail.com>
2020-07-04 15:50:30 +0300

    Интерактивные элементы

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 var selectionHotel = document.querySelector(".form-call-button");
 var formPopup = document.querySelector(".form-container");
+var searchForm = document.querySelector(".search-form");
+var checkinInput = document.querySelector(".checkin-input");
+var checkoutInput = document.querySelector(".checkout-input");
 
 selectionHotel.addEventListener("click", function () {
-  formPopup.classList.toggle("visually-hidden");
+  formPopup.classList.toggle("modal-show");
+});
+
+searchForm.addEventListener("submit", function (evt) {
+  if (!checkinInput.value || !checkoutInput.value) {
+    evt.preventDefault();
+  }
 });
 
 
